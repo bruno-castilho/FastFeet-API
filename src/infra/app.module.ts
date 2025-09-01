7f34d5d1cc8a94af824e541d7358661b5eccb9c8
Bruno da Silva Castilho <bruno.silva.castilho@gmail.com>
2025-09-01 13:28:02 -0300

    feat: configure environment variables

--- src/infra/app.module.ts
+++ src/infra/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { EnvModule } from './env/env.module'
 
 @Module({
-  imports: [],
+  imports: [EnvModule],
   controllers: [AppController],
   providers: [AppService],
 })
