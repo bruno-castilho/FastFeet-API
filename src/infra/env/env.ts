9dbcb40bd4b0624a2df45dc8696aacdce0e27e3a
Bruno da Silva Castilho <bruno.silva.castilho@gmail.com>
2025-09-02 12:11:52 -0300

    feat: added e2e test for create delivery person

--- src/infra/env/env.ts
+++ src/infra/env/env.ts
@@ -2,6 +2,7 @@ import { z } from 'zod'
 
 export const envSchema = z.object({
   PORT: z.coerce.number().optional().default(3333),
+  DATABASE_URL: z.string(),
 })
 
 export type Env = z.infer<typeof envSchema>
