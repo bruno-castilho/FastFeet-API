6b0bfa371ef66689b3760d07ec02dbfe0be3f350
Bruno da Silva Castilho <bruno.silva.castilho@gmail.com>
2025-09-04 15:01:21 -0300

    feat: create storage using R2 Cloudflare

--- src/infra/env/env.ts
+++ src/infra/env/env.ts
@@ -3,6 +3,10 @@ import { z } from 'zod'
 export const envSchema = z.object({
   PORT: z.coerce.number().optional().default(3333),
   DATABASE_URL: z.string(),
+  CLOUDFLARE_ACCOUNT_ID: z.string(),
+  AWS_BUCKET_NAME: z.string(),
+  AWS_ACCESS_KEY_ID: z.string(),
+  AWS_SECRET_ACCESS_KEY: z.string(),
 })
 
 export type Env = z.infer<typeof envSchema>
